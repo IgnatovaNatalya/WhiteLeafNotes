4c2d643b1f401b969f76e259209030eb9d1cb49d
IgnatovaNatalya <ananjeva@gmail.com>
2026-07-22 18:33:12 +0300

    Удаление лишнего кода старого бокового меню

--- app/src/main/java/ru/whiteleaf/notes/presentation/root/DrawerDividerViewHolder.kt
+++
@@ -1,6 +0,0 @@
-package ru.whiteleaf.notes.presentation.root
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-class DrawerDividerViewHolder(view: View) : RecyclerView.ViewHolder(view)
